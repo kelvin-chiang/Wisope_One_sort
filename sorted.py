b201892353f2be9a9d6a2952f4e70aa11f5c9686
kelvin-chiang <752025767@qq.com>
2020-02-01 03:39:14 +0800

    sorted

---
+++ sorted.py
@@ -0,0 +1,15 @@
+corp = ['tencent','alibaba','baidu','wisope']
+# list depend on normal list order
+print("Here is WBAT(sorted order): ")
+print(
+corp[0].title(),
+corp[1].title(),
+corp[2].title(),
+corp[3].title()
+)
+
+# list depend on sorted order
+print("Here is WBAT(common order): ")
+print(sorted(corp))
+
+
