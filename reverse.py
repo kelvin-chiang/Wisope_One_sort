fa3bcb6de90dbaff9d4dff35df8c7a760f544a08
kelvin-chiang <752025767@qq.com>
2020-02-01 03:53:24 +0800

    reverse

---
+++ reverse.py
@@ -0,0 +1,13 @@
+corp = ['alibaba','tencent','baidu','Wisope']
+print("This is printed by normal order: ")
+print(corp)
+# list depend on the reverse order
+corp.reverse()
+print("This is printed by reverse order: ")
+print(
+corp[0].title(),
+corp[1].title(),
+corp[2].title(),
+corp[3].title()
+)
+
